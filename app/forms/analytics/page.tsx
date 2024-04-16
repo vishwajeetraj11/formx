46552c69bf8a2b188e6a5095390e16c58f9e7a4a
vishwajeetraj11 <vishwajeetraj11@gmail.com>
2024-04-17 02:59:19 +0530

    chore: make the carousel dynamic on analytics page

--- app/forms/analytics/page.tsx
+++ app/forms/analytics/page.tsx
@@ -8,10 +8,24 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { BarChartHero } from "@/components/charts/bar-chart";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import Link from "next/link";
 
 type Props = {};
 
-const Analytics = (props: Props) => {
+const Analytics = async (props: Props) => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const forms = await supabase.from("forms").select("*");
+
   return (
     <div className="w-[1200px] mx-auto">
       <h1 className="text-2xl my-8 text-center">Analytics</h1>
@@ -23,23 +37,25 @@ const Analytics = (props: Props) => {
         className="w-full mb-10"
       >
         <CarouselContent>
-          {Array.from({ length: 50 }).map((_, index) => (
-            <CarouselItem key={index} className="basis-1/4">
-              <div className="p-1">
-                <Card>
-                  <CardContent className="flex flex-col items justify-center p-6">
-                    <p className="">School Admission Form</p>
-                    <p className="font-bold">
-                      450<span className="ml-2 font-normal">Responses</span>
-                    </p>
-                    <p className="font-bold">
-                      <span className="mr-2 font-normal">Created At</span>
-                      24th May, 2020
-                    </p>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
+          {forms.data?.map((form, index) => (
+            <Link href={`/forms/${form.id}/analytics`} key={form.id}>
+              <CarouselItem className="basis-1/4">
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex flex-col items justify-center p-6">
+                      <p className="">{form.title}</p>
+                      <p className="font-bold">
+                        450<span className="ml-2 font-normal">Responses</span>
+                      </p>
+                      <p className="font-bold">
+                        <span className="mr-2 font-normal">Created At</span>
+                        {form.created_at.toString()}
+                      </p>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            </Link>
           ))}
         </CarouselContent>
         <CarouselPrevious />
